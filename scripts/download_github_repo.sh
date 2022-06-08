299e9886b675da74a3de33bb313204c55f1b5c30
chen <eeheng.chen@tum.de>
2022-06-08 15:29:15 +0200

    updated install + download scripts

--- scripts/download_github_repo.sh
+++ scripts/download_github_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 
-cd ~/ && sudo rm -rf ./realsense-wrapper-python && wget https://github.com/cheneeheng/realsense-wrapper-python/archive/refs/heads/main.zip && unzip ./main.zip -d . && mv ./realsense-wrapper-python-main ./realsense-wrapper-python && rm ./main.zip
+sudo apt-get update && sudo apt-get install unzip && cd ~/ && sudo rm -rf ./realsense-wrapper-python && wget https://github.com/cheneeheng/realsense-wrapper-python/archive/refs/heads/main.zip && unzip ./main.zip -d . && mv ./realsense-wrapper-python-main ./realsense-wrapper-python && rm ./main.zip
--- scripts/install_realsense_pi4.sh
+++ scripts/install_realsense_pi4.sh
@@ -17,8 +17,11 @@ fi
 lsb_release -a
 echo "Kernel version $(uname -r)"
 
-# Update packages
-sudo apt-get update && sudo apt-get dist-upgrade
+echo "\n--------------------------------------------------------------------------------"
+echo "Update packages"
+echo "--------------------------------------------------------------------------------\n"
+
+sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install -y \
   automake \
   libtool \
@@ -29,9 +32,12 @@ sudo apt-get install -y \
   libglu1-mesa-dev
 # vim \
 
-# Get librealsense source code
+echo "\n--------------------------------------------------------------------------------"
+echo "Get librealsense source code"
+echo "--------------------------------------------------------------------------------\n"
+
 cd ~/
-sudo rm -rf ./librealsense_build
+sudo rm -rf ./librealsense
 git clone https://github.com/IntelRealSense/librealsense.git
 sudo cp librealsense/config/99-realsense-libusb.rules /etc/udev/rules.d/
 
@@ -42,7 +48,9 @@ sudo bash ~/realsense-wrapper-python/scripts/install_realsense_pi4_udev.sh
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 . ~/.bashrc
 
-# Compile protobuff for docs
+echo "\n--------------------------------------------------------------------------------"
+echo "Compile protobuff"
+echo "--------------------------------------------------------------------------------\n"
 # TODO: may not be needed if examples are not built.
 cd ~
 git clone --depth=1 -b v3.10.0 https://github.com/google/protobuf.git
@@ -61,14 +69,20 @@ export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=3
 sudo ldconfig
 protoc --version
 
-# Install libtbb package
+echo "\n--------------------------------------------------------------------------------"
+echo "Install libtbb package"
+echo "--------------------------------------------------------------------------------\n"
+
 cd ~
 wget https://github.com/PINTO0309/TBBonARMv7/raw/master/libtbb-dev_2018U2_armhf.deb
 sudo dpkg -i ~/libtbb-dev_2018U2_armhf.deb
 sudo ldconfig
 rm libtbb-dev_2018U2_armhf.deb
 
-# Compile librealsense
+echo "\n--------------------------------------------------------------------------------"
+echo "Compile librealsense"
+echo "--------------------------------------------------------------------------------\n"
+
 cd ~/librealsense
 mkdir build && cd build
 # cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true
@@ -83,6 +97,10 @@ make -j$(($(nproc) - 1))
 # sudo make install
 
 # export PYTHONPATH=$PYTHONPATH:/home/realsense/librealsense/build
+
+echo "\n--------------------------------------------------------------------------------"
+echo "OpenCV and Numpy"
+echo "--------------------------------------------------------------------------------\n"
 
 cd ~
 . ~/.bashrc
