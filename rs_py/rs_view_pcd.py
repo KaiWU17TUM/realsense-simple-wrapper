00bba3851d755223cf41255984180535a25f99e8
chen <eeheng.chen@tum.de>
2022-06-29 11:11:19 +0200

    Refractored code for viewing raw data in py

--- rs_py/rs_view_pcd.py
+++ rs_py/rs_view_pcd.py
@@ -1,178 +1,138 @@
-import cv2
 import json
 import numpy as np
-import os
 import sys
+
+# import matplotlib.pyplot as plt
+# from mpl_toolkits import mplot3d
+# from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import proj3d
+
+import plotly.graph_objects as go
+import plotly.io as pio
 
 import open3d as o3d
 
-# Currently the code only shows the data from the newest timestamp folder.
+from rs_py.rs_view_raw_data import iterate_over_raw_data
+from rs_py.rs_view_raw_data import read_color_file
+from rs_py.rs_view_raw_data import read_depth_file
 
-# PATH = '/code/realsense-simple-wrapper/output/rs_sample_3dev'
 
-BASE_PATH = sys.argv[1]
+PATH = sys.argv[1]
 FPS = sys.argv[2]
 SCALE = sys.argv[3]
+SYNC = sys.argv[4]
+
+# PATH = '/code/realsense-simple-wrapper/output/rs_sample_3dev'
+# FPS = 5
+# SCALE = 0.75
+# SYNC = 0
 
 
-def get_calib(base_path: str) -> dict:
-    path = {}
-    for device in sorted(os.listdir(base_path)):
-        device_path = os.path.join(base_path, device)
-        for ts in sorted(os.listdir(device_path)):
-            calib_path = os.path.join(base_path, device, ts, 'calib')
-            for filename in sorted(os.listdir(calib_path)):
-                path[device] = os.path.join(calib_path, filename)
-    return path
+def data_process_fn(**kwargs):
 
+    color_files = kwargs['color_files']
+    depth_files = kwargs['depth_files']
+    color_ts_idxs = kwargs['color_ts_idxs']
+    depth_ts_idxs = kwargs['depth_ts_idxs']
+    sync_ts = kwargs['sync_ts']
 
-def get_filepaths(base_path: str, sensor: str) -> dict:
-    out = {}
-    for device in sorted(os.listdir(base_path)):
-        out[device] = []
-        device_path = os.path.join(base_path, device)
-        for ts in sorted(os.listdir(device_path)):
-            sensor_path = os.path.join(base_path, device, ts, sensor)
-            for filename in sorted(os.listdir(sensor_path)):
-                out[device].append(os.path.join(sensor_path, filename))
-    return out
+    pcd_list = []
+    num_dev = len(color_files)
+    for dev_idx in range(num_dev):
+        # (ts, file, calib)
+        color_dc = color_files[dev_idx][color_ts_idxs[dev_idx]]
+        depth_dc = depth_files[dev_idx][depth_ts_idxs[dev_idx]]
 
+        with open(color_dc.calib_file, 'r') as f:
+            calib_data = json.load(f)
 
-def get_filepaths_with_timestamps():
-    dev_color_files = get_filepaths(BASE_PATH, 'color')
-    dev_depth_files = get_filepaths(BASE_PATH, 'depth')
-    dev_calib_file = get_calib(BASE_PATH)
+        h_c = calib_data['color']['height']
+        w_c = calib_data['color']['width']
+        h_d = calib_data['depth']['height']
+        w_d = calib_data['depth']['width']
 
-    num_data = len(tuple(dev_color_files.values())[0])
-    num_dev = len(list(dev_calib_file.keys()))
+        image = read_color_file(color_dc.file)
+        depth = read_depth_file(depth_dc.file)
 
-    devices = list(dev_calib_file.keys())
-    devices = [[i]*num_data for i in devices]
-    calib_files = list(dev_calib_file.values())
-    calib_files = [[i]*num_data for i in calib_files]
+        image = image.reshape(h_c, w_c, 3)
+        depth = depth[-h_d*w_d:].reshape(h_d, w_d)
 
-    color_dict = {dev: {} for dev in dev_calib_file.keys()}
-    depth_dict = {dev: {} for dev in dev_calib_file.keys()}
-    for data_tuple in zip(
-        *tuple(devices),
-        *tuple(calib_files),
-        *tuple(dev_color_files.values()),
-        *tuple(dev_depth_files.values()),
-    ):
-        for i in range(num_dev):
-            dev = data_tuple[0*num_dev+i]
-            calib_file = data_tuple[1*num_dev+i]
-            color_file = data_tuple[2*num_dev+i]
-            depth_file = data_tuple[3*num_dev+i]
+        camera_intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(
+            width=calib_data['depth']['width'],
+            height=calib_data['depth']['height'],
+            fx=calib_data['depth']['intrinsic_mat'][0],
+            fy=calib_data['depth']['intrinsic_mat'][4],
+            cx=calib_data['depth']['intrinsic_mat'][2],
+            cy=calib_data['depth']['intrinsic_mat'][5]
+        )
 
-            color_ts = color_file.split('/')[-1]
-            color_ts = int(color_ts.split('.')[0])
+        pcd = o3d.geometry.PointCloud.create_from_depth_image(
+            depth=o3d.geometry.Image(depth),
+            intrinsic=camera_intrinsic_o3d,
+            # extrinsic=(with default value),
+            depth_scale=1/calib_data['depth']['depth_scale'],
+            depth_trunc=5.0,
+            # stride=1
+        )
 
-            depth_ts = depth_file.split('/')[-1]
-            depth_ts = int(depth_ts.split('.')[0])
+        x = np.asarray(pcd.points)[::10, 0]
+        y = np.asarray(pcd.points)[::10, 1]
+        z = np.asarray(pcd.points)[::10, 2]
 
-            color_dict[dev][color_ts] = [calib_file, color_file]
-            depth_dict[dev][depth_ts] = [calib_file, depth_file]
+        if dev_idx == 1:
+            pio.renderers.default = 'browser'
+            fig = go.Figure(
+                data=[
+                    go.Scatter3d(
+                        x=-x, y=-z, z=-y,
+                        mode='markers',
+                        marker=dict(
+                            size=3,
+                            # color='rgba(255, 0, 0, 0.5)',
+                            color=-z,
+                            colorscale='Jet',
+                            # opacity=0.5
+                        )
+                    )
+                ]
+            )
+            fig.update_layout(
+                title={
+                    'text': f"{depth_file.split('/')[-1]}, "
+                            f"num of points : {z.shape}, "
+                            f"yz swapped, x+y+z negated",
+                    'y': 0.9,
+                    'x': 0.5,
+                    'xanchor': 'center',
+                    'yanchor': 'top'
+                },
+                scene=dict(
+                    xaxis=dict(range=[-4, 4],),
+                    yaxis=dict(range=[0, 6],),
+                    zaxis=dict(range=[-2, 2],),
+                ),
+            )
+            fig.show()
 
-    return color_dict, depth_dict, num_data, num_dev
+        # flip the orientation, so it looks upright, not upside-down
+        pcd.transform([[1, 0, 0, dev_idx*10],
+                       [0, -1, 0, 0],
+                       [0, 0, -1, 0],
+                       [0, 0, 0, 1]])
+        pcd_list.append(pcd)
+
+    o3d.visualization.draw_geometries(
+        pcd_list,
+        window_name='PCD',
+        width=1280,
+        height=768,
+        # zoom=0.3,
+        # front=np.array([0.25, 0.5, 1], dtype=float),
+        # lookat=np.array([15, 0, 0], dtype=float),
+        # up=np.array([0, 1, 0], dtype=float)
+    )
 
 
 if __name__ == "__main__":
 
-    color_dict, depth_dict, num_data, num_dev = get_filepaths_with_timestamps()
-    dev_color_ts = {k: list(v.keys()) for k, v in color_dict.items()}
-    dev_depth_ts = {k: list(v.keys()) for k, v in depth_dict.items()}
-
-    dev_list = list(color_dict.keys())
-
-    ts_color_max = max([list(i.keys())[0] for i in color_dict.values()])
-    ts_depth_max = max([list(i.keys())[0] for i in depth_dict.values()])
-    ts_max = max([ts_color_max, ts_depth_max])
-
-    counter_color = [0] * num_dev
-    counter_depth = [0] * num_dev
-
-    try:
-        while True:
-
-            tmp_color_ts_list = []
-            tmp_depth_ts_list = []
-            for idx, dev in enumerate(dev_list):
-                ts_idx, ts = next(
-                    x
-                    for x in enumerate(dev_color_ts[dev][counter_color[idx]:])
-                    if x[1] >= ts_max
-                )
-                tmp_color_ts_list.append(ts)
-                ts_idx, ts = next(
-                    x
-                    for x in enumerate(dev_depth_ts[dev][counter_depth[idx]:])
-                    if x[1] >= ts_max
-                )
-                tmp_depth_ts_list.append(ts)
-            ts_max = max(tmp_color_ts_list + tmp_depth_ts_list)
-
-            pcd_list = []
-            for i, dev in enumerate(dev_list):
-                calib_file = color_dict[dev][tmp_color_ts_list[i]][0]
-                color_file = color_dict[dev][tmp_color_ts_list[i]][1]
-                depth_file = depth_dict[dev][tmp_depth_ts_list[i]][1]
-
-                with open(calib_file, 'r') as f:
-                    calib_data = json.load(f)
-
-                h_c = calib_data['color']['height']
-                w_c = calib_data['color']['width']
-                h_d = calib_data['depth']['height']
-                w_d = calib_data['depth']['width']
-
-                if color_file.endswith('.bin'):
-                    with open(color_file, 'rb') as f:
-                        img = np.fromfile(f, np.uint8).reshape(h_c, w_c, 3)
-                else:
-                    img = np.load(color_file).reshape(h_c, w_c, 3)
-                # img = np.flip(img, -1)
-
-                if depth_file.endswith('.bin'):
-                    with open(depth_file, 'rb') as f:
-                        depth = np.fromfile(f, np.uint16)[-h_d*w_d:]
-                        depth = depth.reshape(h_d, w_d)
-                else:
-                    depth = np.fromfile(depth_file, np.uint16)[-h_d*w_d:]
-                    depth = depth.reshape(h_d, w_d)
-
-                # rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
-                #     color, depth, convert_rgb_to_intensity=False)
-                # pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
-                #     rgbd, pinhole_camera_intrinsic)
-                camera_intrinsic_o3d = o3d.camera.PinholeCameraIntrinsic(
-                    width=calib_data['depth']['width'],
-                    height=calib_data['depth']['height'],
-                    fx=calib_data['depth']['intrinsic_mat'][0],
-                    fy=calib_data['depth']['intrinsic_mat'][4],
-                    cx=calib_data['depth']['intrinsic_mat'][2],
-                    cy=calib_data['depth']['intrinsic_mat'][5]
-                )
-
-                pcd = o3d.geometry.PointCloud.create_from_depth_image(
-                    depth=o3d.geometry.Image(depth),
-                    intrinsic=camera_intrinsic_o3d,
-                    # extrinsic=(with default value),
-                    depth_scale=1/calib_data['depth']['depth_scale'],
-                    depth_trunc=5.0,
-                    # stride=1
-                )
-
-                # flip the orientation, so it looks upright, not upside-down
-                pcd.transform([[1, 0, 0, i*10], [0, -1, 0, 0],
-                               [0, 0, -1, 0], [0, 0, 0, 1]])
-                pcd_list.append(pcd)
-
-            o3d.visualization.draw_geometries(pcd_list, height=768, width=1280)
-
-    except Exception as e:
-        print(e)
-        exit(1)
-
-    finally:
-        exit(0)
+    iterate_over_raw_data(PATH, SYNC, data_process_fn=data_process_fn)
--- rs_py/rs_view_raw_data.py
+++ rs_py/rs_view_raw_data.py
@@ -4,165 +4,276 @@ import numpy as np
 import os
 import sys
 
-# Currently the code only shows the data from the newest timestamp folder.
+from typing import Tuple, Optional
 
-BASE_PATH = sys.argv[1]
-FPS = sys.argv[2]
+
+# PATH = sys.argv[1]
+# FPS = sys.argv[2]
+# SCALE = sys.argv[3]
+# SYNC = sys.argv[4]
+
+PATH = '/code/realsense-simple-wrapper/output/rs_sample_3dev'
+FPS = 5
+SCALE = 0.75
+SYNC = 1
+
+
+class DataContainer:
+    def __init__(self,
+                 file=None,
+                 calib_file=None,
+                 timestamp=None,
+                 device_sn=None) -> None:
+        self.file = file
+        self.calib_file = calib_file
+        self.timestamp = timestamp
+        self.device_sn = device_sn
 
 
 # https://github.com/IntelRealSense/librealsense/issues/4646
-def get_brg(data_array):
+def _get_brg_from_yuv(data_array: np.ndarray) -> np.ndarray:
     # Input: Intel handle to 16-bit YU/YV data
     # Output: BGR8
     UV = np.uint8(data_array >> 8)
     Y = np.uint8((data_array << 8) >> 8)
-    YUV = np.zeros((*(data_array.shape), 2), 'uint8')
-    YUV[:, :, 0] = Y
-    YUV[:, :, 1] = UV
+    YUV = np.zeros((data_array.shape[0], 2), 'uint8')
+    YUV[:, 0] = Y
+    YUV[:, 1] = UV
     BGR = cv2.cvtColor(YUV, cv2.COLOR_YUV2BGR_YUYV)
     return BGR
 
 
-def get_calib(base_path: str) -> dict:
-    path = {}
-    for device in sorted(os.listdir(base_path)):
-        device_path = os.path.join(base_path, device)
-        for ts in sorted(os.listdir(device_path)):
-            calib_path = os.path.join(base_path, device, ts, 'calib')
-            for filename in sorted(os.listdir(calib_path)):
-                path[device] = os.path.join(calib_path, filename)
-    return path
+def read_color_file(color_file: str) -> np.ndarray:
+    if color_file.endswith('.bin'):
+        with open(color_file, 'rb') as f:
+            image = np.fromfile(f, np.uint8)
+    else:
+        image = np.load(color_file)
+        if image.dtype == np.dtype(np.uint8):
+            pass
+        elif image.dtype == np.dtype(np.uint16):
+            image = _get_brg_from_yuv(image.reshape(-1))
+        else:
+            raise ValueError("Unknown data type :", image.dtype)
+    return image
+
+
+def read_depth_file(depth_file: str) -> np.ndarray:
+    if depth_file.endswith('.bin'):
+        with open(depth_file, 'rb') as f:
+            depth = np.fromfile(f, np.uint16)
+    else:
+        depth = np.fromfile(depth_file, np.uint16)
+    return depth
 
 
 def get_filepaths(base_path: str, sensor: str) -> dict:
-    out = {}
+    path = {}
     for device in sorted(os.listdir(base_path)):
-        out[device] = []
+        path[device] = {}
         device_path = os.path.join(base_path, device)
         for ts in sorted(os.listdir(device_path)):
+            path[device][ts] = []
             sensor_path = os.path.join(base_path, device, ts, sensor)
             for filename in sorted(os.listdir(sensor_path)):
-                out[device].append(os.path.join(sensor_path, filename))
-    return out
+                path[device][ts].append(os.path.join(sensor_path, filename))
+    return path
 
 
-def get_filepaths_with_timestamps():
-    dev_color_files = get_filepaths(BASE_PATH, 'color')
-    dev_depth_files = get_filepaths(BASE_PATH, 'depth')
-    dev_calib_file = get_calib(BASE_PATH)
+def get_filepaths_with_timestamps(base_path: str) -> Tuple[dict, dict, list]:
+    # dev > trial(as timestamp) > files(with timestamps in the name)
+    dev_trial_color_filepaths = get_filepaths(base_path, 'color')
+    dev_trial_depth_filepaths = get_filepaths(base_path, 'depth')
+    dev_trial_calib_filepaths = get_filepaths(base_path, 'calib')
 
-    num_data = len(tuple(dev_color_files.values())[0])
-    num_dev = len(list(dev_calib_file.keys()))
+    def _get_trial_list(dev_trial_x_filepaths: dict) -> list:
+        out = []
+        for trial_x_filepaths in dev_trial_x_filepaths.values():
+            for trial in trial_x_filepaths.keys():
+                if trial not in out:
+                    out.append(trial)
+        return out
 
-    devices = list(dev_calib_file.keys())
-    devices = [[i]*num_data for i in devices]
-    calib_files = list(dev_calib_file.values())
-    calib_files = [[i]*num_data for i in calib_files]
+    trial_list = list(set(_get_trial_list(dev_trial_color_filepaths) +
+                          _get_trial_list(dev_trial_depth_filepaths)))
 
-    color_dict = {dev: {} for dev in dev_calib_file.keys()}
-    depth_dict = {dev: {} for dev in dev_calib_file.keys()}
-    for data_tuple in zip(
-        *tuple(devices),
-        *tuple(calib_files),
-        *tuple(dev_color_files.values()),
-        *tuple(dev_depth_files.values()),
-    ):
-        for i in range(num_dev):
-            dev = data_tuple[0*num_dev+i]
-            calib_file = data_tuple[1*num_dev+i]
-            color_file = data_tuple[2*num_dev+i]
-            depth_file = data_tuple[3*num_dev+i]
+    def _ts_from_filepath(path: str) -> int:
+        ts = path.split('/')[-1]
+        ts = int(ts.split('.')[0])
+        return ts
 
-            color_ts = color_file.split('/')[-1]
-            color_ts = int(color_ts.split('.')[0])
+    # trial(as timestamp) > dev > timestamps > [filepath, calib]
+    color_dict = {trial: {} for trial in trial_list}
+    for dev, trial_color_filepaths in dev_trial_color_filepaths.items():
+        for trial, files in trial_color_filepaths.items():
+            color_dict[trial][dev] = {
+                _ts_from_filepath(file):
+                [file, dev_trial_calib_filepaths[dev][trial][0]]
+                for file in files
+            }
+    depth_dict = {trial: {} for trial in trial_list}
+    for dev, trial_depth_filepaths in dev_trial_depth_filepaths.items():
+        for trial, files in trial_depth_filepaths.items():
+            depth_dict[trial][dev] = {
+                _ts_from_filepath(file):
+                [file, dev_trial_calib_filepaths[dev][trial][0]]
+                for file in files
+            }
 
-            depth_ts = depth_file.split('/')[-1]
-            depth_ts = int(depth_ts.split('.')[0])
+    return color_dict, depth_dict, trial_list
 
-            color_dict[dev][color_ts] = [calib_file, color_file]
-            depth_dict[dev][depth_ts] = [calib_file, depth_file]
 
-    return color_dict, depth_dict, num_data, num_dev
+def iterate_over_raw_data(base_path: str,
+                          sync_ts: bool,
+                          data_process_fn: Optional[callable] = None):
+
+    # trial(as timestamp) > dev > timestamps > [filepath, calib]
+    color_dict, depth_dict, trial_list = \
+        get_filepaths_with_timestamps(base_path)
+
+    # 1. loop through each trial
+    for trial in trial_list:
+        # #dev
+        color_files, depth_files = [], []
+
+        # 2. prepare data tuple, collect all devices of same trial together
+        # dev > #frames > (ts, file, calib)
+        for data_tuple in zip(color_dict[trial].items(),
+                              depth_dict[trial].items()):
+            dev = data_tuple[0][0]
+            _color_files = [
+                DataContainer(*files, ts, dev)
+                for ts, files in data_tuple[0][1].items()
+            ]
+            _depth_files = [
+                DataContainer(*files, ts, dev)
+                for ts, files in data_tuple[1][1].items()
+            ]
+            color_files.append(_color_files)
+            depth_files.append(_depth_files)
+
+        if sync_ts == 0:
+            ts_max = 0
+        elif sync_ts == 1:
+            ts_color_max = max([i[0].timestamp for i in color_files])
+            ts_depth_max = max([i[0].timestamp for i in depth_files])
+            ts_max = max([ts_color_max, ts_depth_max])
+            counter_color = [0] * len(color_files)
+            counter_depth = [0] * len(depth_files)
+        else:
+            raise ValueError("Unknown SYNC mode...")
+
+        try:
+            while True:
+
+                if sync_ts == 0:
+                    _color_ts_idxs = [ts_max for _ in range(len(color_files))]
+                    _depth_ts_idxs = [ts_max for _ in range(len(depth_files))]
+                    ts_max += 1
+
+                elif sync_ts == 1:
+                    _color_ts = []
+                    _depth_ts = []
+                    _color_ts_idxs = []
+                    _depth_ts_idxs = []
+                    for idx, file in enumerate(color_files):
+                        ts_idx, ts = next(
+                            (i, dc.timestamp)
+                            for i, dc in enumerate(file[counter_color[idx]:])
+                            if dc.timestamp >= ts_max
+                        )
+                        _color_ts.append(ts)
+                        _color_ts_idxs.append(ts_idx)
+                    for idx, file in enumerate(depth_files):
+                        ts_idx, ts = next(
+                            (i, dc.timestamp)
+                            for i, dc in enumerate(file[counter_depth[idx]:])
+                            if dc.timestamp >= ts_max
+                        )
+                        _depth_ts.append(ts)
+                        _depth_ts_idxs.append(ts_idx)
+                    ts_max = max(_color_ts + _depth_ts)
+
+                if data_process_fn is not None:
+                    data_process_fn(color_files=color_files,
+                                    depth_files=depth_files,
+                                    color_ts_idxs=_color_ts_idxs,
+                                    depth_ts_idxs=_depth_ts_idxs,
+                                    sync_ts=sync_ts,
+                                    )
+
+        except Exception as e:
+            print(e)
+            exit(1)
+
+        finally:
+            exit(0)
+
+
+def data_process_fn(**kwargs):
+
+    color_files = kwargs['color_files']
+    depth_files = kwargs['depth_files']
+    color_ts_idxs = kwargs['color_ts_idxs']
+    depth_ts_idxs = kwargs['depth_ts_idxs']
+    sync_ts = kwargs['sync_ts']
+
+    imgs = []
+    num_dev = len(color_files)
+    for dev_idx in range(num_dev):
+        # (ts, file, calib)
+        color_dc = color_files[dev_idx][color_ts_idxs[dev_idx]]
+        depth_dc = depth_files[dev_idx][depth_ts_idxs[dev_idx]]
+
+        with open(color_dc.calib_file, 'r') as f:
+            calib_data = json.load(f)
+
+        h_c = calib_data['color']['height']
+        w_c = calib_data['color']['width']
+        h_d = calib_data['depth']['height']
+        w_d = calib_data['depth']['width']
+
+        image = read_color_file(color_dc.file)
+        depth = read_depth_file(depth_dc.file)
+
+        image = image.reshape(h_c, w_c, 3)
+        depth = depth[-h_d*w_d:].reshape(h_d, w_d)
+
+        depth = cv2.applyColorMap(
+            cv2.convertScaleAbs(depth, alpha=0.03),
+            cv2.COLORMAP_JET
+        )
+
+        cv2.putText(image,
+                    f"{color_dc.device_sn} - {color_dc.timestamp}",
+                    (10, 30),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    2,
+                    (0, 255, 0),
+                    2,
+                    cv2.LINE_AA)
+        cv2.putText(image,
+                    f"{depth_dc.device_sn} - {depth_dc.timestamp}",
+                    (10, 80),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    2,
+                    (0, 255, 0),
+                    2,
+                    cv2.LINE_AA)
+        imgs.append(np.vstack([image, depth]))
+
+    output = np.hstack(imgs)
+    output = cv2.resize(output, (int(output.shape[1]*SCALE),
+                                 int(output.shape[0]*SCALE)))
+
+    name = f'output - sync={sync_ts}'
+    cv2.namedWindow(name)
+    cv2.moveWindow(name, 0, 0)
+    cv2.imshow(name, output)
+    # cv2.waitKey(1000//FPS)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
 
-    dev_color_files = get_filepaths(PATH, 'color')
-    dev_depth_files = get_filepaths(PATH, 'depth')
-    dev_calib_file = get_calib(PATH)
-
-    num_data = len(tuple(dev_color_files.values())[0])
-    num_dev = len(list(dev_calib_file.keys()))
-
-    devices = list(dev_calib_file.keys())
-    devices = [[i]*num_data for i in devices]
-    calib_files = list(dev_calib_file.values())
-    calib_files = [[i]*num_data for i in calib_files]
-
-    color_dict = {dev: {} for dev in dev_calib_file.keys()}
-    depth_dict = {dev: {} for dev in dev_calib_file.keys()}
-    for data_tuple in zip(
-        *tuple(devices),
-        *tuple(calib_files),
-        *tuple(dev_color_files.values()),
-        *tuple(dev_depth_files.values()),
-    ):
-
-        imgs = []
-        for i in range(num_dev):
-            dev = data_tuple[0*num_dev+i]
-            calib_file = data_tuple[1*num_dev+i]
-            color_file = data_tuple[2*num_dev+i]
-            depth_file = data_tuple[3*num_dev+i]
-
-            color_ts = color_file.split('/')[-1]
-            color_ts = int(color_ts.split('.')[0])
-
-            depth_ts = depth_file.split('/')[-1]
-            depth_ts = int(depth_ts.split('.')[0])
-
-            with open(calib_file, 'r') as f:
-                calib_data = json.load(f)
-
-            h_c = calib_data['color']['height']
-            w_c = calib_data['color']['width']
-            h_d = calib_data['depth']['height']
-            w_d = calib_data['depth']['width']
-
-            if color_file.endswith('.bin'):
-                with open(color_file, 'rb') as f:
-                    img = np.fromfile(f, np.uint8).reshape(h_c, w_c, 3)
-            else:
-                img = np.load(color_file)
-                if img.dtype == np.dtype(np.uint8):
-                    img = img.reshape(h_c, w_c, 3)
-                elif img.dtype == np.dtype(np.uint16):
-                    img = get_brg(img.reshape(h_c, w_c))
-                else:
-                    raise ValueError("Unknown data type :", img.dtype)
-            # img = np.flip(img, -1)
-
-            if depth_file.endswith('.bin'):
-                with open(depth_file, 'rb') as f:
-                    depth = np.fromfile(f, np.uint16)[-h_d*w_d:]
-                    depth = depth.reshape(h_d, w_d)
-            else:
-                depth = np.fromfile(depth_file, np.uint16)[-h_d*w_d:]
-                depth = depth.reshape(h_d, w_d)
-            depth = cv2.applyColorMap(
-                cv2.convertScaleAbs(depth, alpha=0.03),
-                cv2.COLORMAP_JET
-            )
-
-            cv2.putText(img, str(color_ts),
-                        (10, 30), cv2.FONT_HERSHEY_PLAIN,
-                        2, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(img, str(depth_ts),
-                        (10, 80), cv2.FONT_HERSHEY_PLAIN,
-                        2, (0, 255, 0), 2, cv2.LINE_AA)
-            imgs.append(np.vstack([img, depth]))
-
-        output = np.hstack(imgs)
-        cv2.imshow('output', output)
-        # cv2.waitKey(1000//FPS)
-        cv2.waitKey(0)
+    iterate_over_raw_data(PATH, SYNC, data_process_fn=data_process_fn)
