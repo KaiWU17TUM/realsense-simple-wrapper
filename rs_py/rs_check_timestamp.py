9f6f553dfbf9c3517350932c60cebd70d80fab1e
chen <eeheng.chen@tum.de>
2022-06-29 11:11:36 +0200

    Refractored code for ts check

--- rs_py/rs_check_timestamp.py
+++ rs_py/rs_check_timestamp.py
@@ -1,31 +1,31 @@
-import os
 import sys
 
-BASE_PATH = sys.argv[1]
+from rs_py.utils import get_filepaths
 
+PATH = sys.argv[1]
 
-def get_timestamp(base_path):
-    path = {}
-    for device in sorted(os.listdir(base_path)):
-        device_path = os.path.join(base_path, device)
-        for ts in sorted(os.listdir(device_path)):
-            calib_path = os.path.join(base_path, device, ts, 'timestamp')
-            for filename in sorted(os.listdir(calib_path)):
-                path[device] = os.path.join(calib_path, filename)
-    return path
+# PATH = '/code/realsense-simple-wrapper/output/rs_sample_3dev'
 
 
 if __name__ == "__main__":
 
-    dev_ts_file_path = get_timestamp(BASE_PATH)
+    dev_trial_ts_filepaths = get_filepaths(PATH, 'timestamp')
 
-    for dev, ts_file_path in dev_ts_file_path.items():
-        with open(ts_file_path, 'r') as f:
-            lines = f.readlines()
+    for dev, trial_ts_filepath in dev_trial_ts_filepaths.items():
+        for trial, ts_filepath in trial_ts_filepath.items():
 
-        c_ts = [float(i.split("::")[0]) for i in lines]
-        d_ts = [float(i.split("::")[1]) for i in lines]
+            ts_filepath = ts_filepath[0]
+            with open(ts_filepath, 'r') as f:
+                lines = f.readlines()
 
-        print(f"Device    : {dev}")
-        print(f"Color FPS : {float(len(c_ts))/((c_ts[-1]-c_ts[0])/1e6):.2f}")
-        print(f"Depth FPS : {float(len(d_ts))/((d_ts[-1]-d_ts[0])/1e6):.2f}")
+            c_ts = [float(i.split("::")[0]) for i in lines]
+            d_ts = [float(i.split("::")[1]) for i in lines]
+
+            c_fps = float(len(c_ts))/((c_ts[-1]-c_ts[0])/1e6)
+            d_fps = float(len(d_ts))/((d_ts[-1]-d_ts[0])/1e6)
+
+            print("-"*40)
+            print(f"Trial     : {trial}")
+            print(f"Device    : {dev}")
+            print(f"Color FPS : {c_fps:.2f}")
+            print(f"Depth FPS : {d_fps:.2f}")
