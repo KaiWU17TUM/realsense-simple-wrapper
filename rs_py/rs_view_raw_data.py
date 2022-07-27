4c2eb61c86ac04a6e5e96b017954ef7cc7d51806
chen <eeheng.chen@tum.de>
2022-07-27 16:43:16 +0200

    updated raw data viewer for reading calib

--- rs_py/rs_view_raw_data.py
+++ rs_py/rs_view_raw_data.py
@@ -1,4 +1,5 @@
 import cv2
+import csv
 import json
 import numpy as np
 import os
@@ -21,6 +22,54 @@ class DataContainer:
         self.device_sn = device_sn
 
 
+def read_calib_file(calib_file: str) -> dict:
+    calib_data = {}
+    if calib_file.endswith('.json'):
+        with open(calib_file, 'r') as f:
+            calib_data = json.load(f)
+    elif calib_file.endswith('.csv'):
+        with open(calib_file, 'r') as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    calib_data['color'] = {
+                        'width': int(row[0]),
+                        'height': int(row[1]),
+                        'ppx': float(row[2]),
+                        'ppy': float(row[3]),
+                        'fx': float(row[4]),
+                        'fy': float(row[5]),
+                        'model': row[6],
+                        'coeffs': [float(i) for i in row[7:12]],
+                        'format': row[12],
+                        'fps': int(row[13]),
+                    }
+                if line_count == 1:
+                    calib_data['depth'] = {
+                        'width': int(row[0]),
+                        'height': int(row[1]),
+                        'ppx': float(row[2]),
+                        'ppy': float(row[3]),
+                        'fx': float(row[4]),
+                        'fy': float(row[5]),
+                        'model': row[6],
+                        'coeffs': [float(i) for i in row[7:12]],
+                        'format': row[12],
+                        'fps': int(row[13]),
+                    }
+                if line_count == 2:
+                    calib_data['T_color_depth'] = {
+                        'rotation': [float(i) for i in row[0:9]],
+                        'translation': [float(i) for i in row[9:12]],
+                    }
+                if line_count == 3:
+                    calib_data['depth']['depth_scale'] = float(row[0])
+                    calib_data['depth']['depth_baseline'] = float(row[1])
+                line_count += 1
+    return calib_data
+
+
 # https://github.com/IntelRealSense/librealsense/issues/4646
 def _get_brg_from_yuv(data_array: np.ndarray) -> np.ndarray:
     # Input: Intel handle to 16-bit YU/YV data
@@ -36,12 +85,19 @@ def _get_brg_from_yuv(data_array: np.ndarray) -> np.ndarray:
     return BGR
 
 
-def read_color_file(color_file: str) -> np.ndarray:
-    if color_file.endswith('.bin'):
-        with open(color_file, 'rb') as f:
-            image = np.fromfile(f, np.uint8)
+def read_color_file(filename: str,
+                    fileformat: Optional[str] = None) -> np.ndarray:
+    if filename.endswith('.bin'):
+        with open(filename, 'rb') as f:
+            if fileformat in ['bgr8', 'rgb8']:
+                image = np.fromfile(f, np.uint8)
+            elif fileformat in ['yuyv']:
+                image = np.fromfile(f, np.uint16)
+                image = _get_brg_from_yuv(image.reshape(-1))
+            else:
+                raise ValueError("Unknown data type :", image.dtype)
     else:
-        image = np.load(color_file)
+        image = np.load(filename)
         if image.dtype == np.dtype(np.uint8):
             pass
         elif image.dtype == np.dtype(np.uint16):
@@ -51,12 +107,12 @@ def read_color_file(color_file: str) -> np.ndarray:
     return image
 
 
-def read_depth_file(depth_file: str) -> np.ndarray:
-    if depth_file.endswith('.bin'):
-        with open(depth_file, 'rb') as f:
+def read_depth_file(filename: str) -> np.ndarray:
+    if filename.endswith('.bin'):
+        with open(filename, 'rb') as f:
             depth = np.fromfile(f, np.uint16)
     else:
-        depth = np.fromfile(depth_file, np.uint16)
+        depth = np.fromfile(filename, np.uint16)
     return depth
 
 
@@ -224,15 +280,15 @@ def data_process_fn(**kwargs):
         color_dc = color_files[dev_idx][color_ts_idxs[dev_idx]]
         depth_dc = depth_files[dev_idx][depth_ts_idxs[dev_idx]]
 
-        with open(color_dc.calib_file, 'r') as f:
-            calib_data = json.load(f)
+        calib_data = read_calib_file(color_dc.calib_file)
 
         h_c = calib_data['color']['height']
         w_c = calib_data['color']['width']
         h_d = calib_data['depth']['height']
         w_d = calib_data['depth']['width']
 
-        image = read_color_file(color_dc.file)
+        image = read_color_file(color_dc.file,
+                                calib_data['color'].get('format', None))
         depth = read_depth_file(depth_dc.file)
 
         image = image.reshape(h_c, w_c, 3)
@@ -280,7 +336,7 @@ if __name__ == "__main__":
     PATH = sys.argv[1]
     SYNC = int(sys.argv[2])
     FPS = int(sys.argv[3])
-    SCALE = int(sys.argv[4])
+    SCALE = float(sys.argv[4])
 
     # PATH = '/code/realsense-simple-wrapper/output/calibration_848_480'
     # SYNC = 1
